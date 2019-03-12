05822fa2d2e32607f2b0262aebeb91a9dc3bb0a5
github username <github email address>
2019-03-12 15:17:33 +0000

    starwars-api
    edits to 3 files

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get('http://www.swapi.co/api/people/.json')
   response_hash = JSON.parse(response_string)
 
   # iterate over the response hash to find the collection of `films` for the given
@@ -16,10 +16,31 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  test = response_hash["results"].find {|hash| hash["name"] == character_name}
 end
 
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+def intialise_input(character_name)
+  name = []
+  character_name
+end
+
+def find_films(character_name)
+  result = get_character_movies_from_api(character_name)
+  film_names = result["films"]
+end
+
+def film_title(url)
+response_string = RestClient.get(url)
+response_hash = JSON.parse(response_string)
+end
+
+def grab(url)
+ title =  film_title(url)["title"]
+ puts "Title: #{title}"
+end
+
+def all_films(character_name)
+  find_films(character_name).collect {|url| grab(url)}
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome, how are you"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  result = gets.chomp
+  result.split.map {|x| x.capitalize!}.join(" ")
 end
